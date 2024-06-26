ad9820a4d148cd57aa5e3cfcb3a67710c876771a
amwaggoner <amwaggoner10@gmail.com>
2024-06-26 12:55:43 -0500

    Acsys implementation still in progress

--- Peak_Finder.py
+++ Peak_Finder.py
@@ -6,8 +6,12 @@ import csv
 import datetime
 import acsys
 import acsys.dpm
+import acsys.sync
 import aiostream
+import asyncio
 import logging
+import argparse
+import os
 
 datadir = r'\Users\waggoner\Downloads'
 hardcodefile = r'%s\6-21-2024_1200V_Ev17_4_ALL.csv'%datadir
@@ -32,27 +36,72 @@ log = logging.getLogger('acsys')
 log.setLevel(logging.INFO)
 log.info('script started')
 
-async def my_app(con):
-    devlist = dev_list()
+'''async def my_app(con):
+    #devlist = dev_list()
     async with acsys.dpm.DPMContext(con) as dpmquery:
         log.info('adding entries')
-        await dpmquery.add_entries(list(enumerate(devlist)))
-            
-        log.info('starting queries')
+        await dpmquery.add_entry(0, 'Z:CUBE_Z@i')
+        
+        log.info('starting dpm')
         await dpmquery.start()
         
-        async for ii in dpmquery.replies():
-            print(str(ii))
+        async for reply in dpmquery:
+            log.info(str(reply.data))
+    async for ii in acsys.sync.get_events(con, ['e,53']):
+        log.info('%s',str(ii))'''
     
+def main():
+
+    args_dict = {'readfile':'', 'event':'', 'dev_list':[],'role':''}
+    parse_args(args_dict)
+    
+    sc = scanner()
+
+    read_list = args_dict['dev_list'] + sc.readList(args_dict['readfile'])
+    ramp_list = make_ramplist(sc,args_dict['dev_list'])
+    #ramp_list=[]
+    Nmeas = 1 # number of measurements to be taken at every setting
+    
+    devs = dev_list()
+    nominals = sc.get_settings_once(devs)
+    fig, ax = plt.subplots(1,2,figsize=(9,5))
+    fig.suptitle("PMT trace")
+    fig.supxlabel('time (s)')
+    fig.supylabel('signal (V)')
+    ax[0].plot(range(len(nominals)),nominals,label='PMT Data')
+    ax[0].set_title('Raw')
+    ax[0].legend()
+    plt.show()
+    
+    print(nominals)
     
 def dev_list():
     output = []
-    for i in range(1):
-        output.append('L:PMT10W[{i}]')
+    for i in range(3999):
+        output.append('L:PMT10W[%s]'%str(i))
     
     return output
 
+def parse_args(args_dict):
+    parser = argparse.ArgumentParser(description="usage: %prog [options] \n")
+    parser.add_argument ('--readfile',  dest='readfile', default='Reading_devices.csv',
+                         help="Reading device list file name. (default: Reading_devices.csv)")
+    parser.add_argument ('--devlist',  dest='devlist', default=['L:QPS101'],
+                         help="List of devices to scan. (default: Z:CUBE devices")
+    parser.add_argument ('--event',  dest='event', default='@e,1d,e,0',
+                         help="Event or periodic frequency. (default: '@p,1000')")
+    parser.add_argument ('--role',  dest='role', default='testing',
+                         help="Setting role. (default: 'testing')")
+    
+    
+    options  = parser.parse_args()
 
+    #assume files are in workdir
+    args_dict['readfile'] = os.path.join(os.getcwd(),options.readfile)
+    args_dict['event']    = options.event
+    args_dict['dev_list'] = options.devlist
+    args_dict['role']     = options.role
+    
 def run_script(inputfile):
     ##Extract Data
     file = io.open(inputfile)
@@ -172,6 +221,71 @@ def process_data(process_channel,time_channel):
 
     return process_channel
     
+def make_ramplist(sc,device_list):
+    nominals = sc.get_settings_once(device_list)
+
+    ramplist = []
+
+    [ramplist.append(sum([[dev,float(nom)+i] for dev,nom in zip(device_list,nominals)],['1'])) for i in range(-3,4)]
+    ramplist[0][0]='0'
+
+    #print(ramplist)
+    return ramplist
+    
+class scanner:
+    def __init__(self):
+        self.thread_dict = {}
+    
+    def get_settings_once(self,paramlist):
+        if paramlist and len(paramlist)!=0:
+            drf_list = self.build_set_device_list(paramlist)
+        else:
+            print('Device list empty - abort')
+            return
+        nominals= acsys.run_client(read_once, drf_list=drf_list)
+        return nominals
+    def readList(self,filename):
+        try:
+            file = open(r'%s'%filename)
+            lines = file.readlines()
+            read_list = []
+            for line in lines:
+                if line.find('//') !=-1:
+                    continue
+                devs = [dev.strip('\n') for dev in line.split(',') if (dev.find(':')!=-1 or dev.find('_')!=-1) and isinstance(dev,str)] 
+                [read_list.append(dev) for dev in devs]
+
+        except:
+            read_list = []
+            print('Read device list empty')
+        return read_list   
+    def build_set_device_list(self,devlist):
+        drf_list=[]
+        for dev in devlist:
+            drf = f'{dev}'
+            drf_list.append(drf)
+            
+        return drf_list
+    
+async def read_once(con,drf_list):
+
+    settings = [None]*len(drf_list)
+    async with acsys.dpm.DPMContext(con) as dpm:
+        for i in range(len(drf_list)):
+            await dpm.add_entry(i, drf_list[i]+'@i')
+
+            await dpm.start()
+
+        async for reply in dpm:
+            settings[reply.tag]=reply.data
+            #settings[reply.tag] = 1
+            #print(reply.data)
+            if settings.count(None) ==0:
+                break
+    return settings
+    
 log.info('running client')
-acsys.run_client(my_app)
+'''acsys.run_client(my_app)'''
 ##run_script(hardcodefile)
+if __name__ == "__main__":
+    main()
